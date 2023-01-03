75dda007d1b4e96b402b752e2014f8879febdb31
Javier Rodriguez Juarez <atarax.ia@outlook.com>
2023-01-03 09:42:41 +0100

    MS Configuracion

---
+++ Configuracion/configuracion/src/test/java/ia/atarax/configuracion/ConfiguracionApplicationTests.java
@@ -0,0 +1,13 @@
+package ia.atarax.configuracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
