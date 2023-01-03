c89d980b3cb63345d533951a24f4c0aa0a47038d
Javier Rodriguez Juarez <atarax.ia@outlook.com>
2023-01-03 13:26:16 +0100

    MS Circuit Breaker

--- Web/web/src/main/java/ia/atarax/web/service/RemoteLocalidadService.java
+++ Web/web/src/main/java/ia/atarax/web/service/RemoteLocalidadService.java
@@ -1,6 +1,7 @@
 package ia.atarax.web.service;
 
 import ia.atarax.web.entity.Localidad;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,21 +23,28 @@ public class RemoteLocalidadService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @CircuitBreaker(name = "locListBreaker", fallbackMethod = "getListLocalidadInfoFallBack")
     public List<Localidad> findAllLocalidad() {
         log.info("LOCALIDAD SERVICIO REMOTO: Búsqueda de todas las localidades");
         ResponseEntity<Localidad[]> localidadResponse = this.restTemplate.getForEntity(endpoint, Localidad[].class);
         return Arrays.asList(localidadResponse.getBody());
     }
 
+    @CircuitBreaker(name = "locListBreaker", fallbackMethod = "getListLocalidadInfoFallBack")
     public List<Localidad> findLocalidadByNombre(String nombre) {
         log.info("LOCALIDAD SERVICIO REMOTO: Búsqueda de las localidades por nombre que contengan: '" + nombre + "'");
         ResponseEntity<Localidad[]> listaL = this.restTemplate.getForEntity(endpoint + "/name/{nombre}", Localidad[].class, nombre);
         return Arrays.asList(listaL.getBody());
     }
 
+    @CircuitBreaker(name = "locListBreaker", fallbackMethod = "getListLocalidadInfoFallBack")
     public List<Localidad> findLocalidadByCodProvincia(String cod) {
         log.info("LOCALIDAD SERVICIO REMOTO: Búsqueda de las localidades por provincia");
         ResponseEntity<Localidad[]> listaL = this.restTemplate.getForEntity(endpoint + "/prov/{cod}", Localidad[].class, cod);
         return Arrays.asList(listaL.getBody());
+    }
+
+    public List<Localidad> getListLocalidadInfoFallBack() {
+        return new ArrayList<>();
     }
 }
--- Web/web/src/main/java/ia/atarax/web/service/RemoteProvinciaService.java
+++ Web/web/src/main/java/ia/atarax/web/service/RemoteProvinciaService.java
@@ -1,6 +1,7 @@
 package ia.atarax.web.service;
 
 import ia.atarax.web.entity.Provincia;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,27 +23,39 @@ public class RemoteProvinciaService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @CircuitBreaker(name = "provListBreaker", fallbackMethod = "getListProvinciaInfoFallBack")
     public List<Provincia> findAllProvincia() {
         log.info("PROVINCIA SERVICIO REMOTO: Búsqueda de todas las provincias");
         ResponseEntity<Provincia[]> provinciaResponse = this.restTemplate.getForEntity(endpoint, Provincia[].class);
         return Arrays.asList(provinciaResponse.getBody());
     }
 
+    @CircuitBreaker(name = "provBreaker", fallbackMethod = "getProvinciaInfoFallBack")
     public Provincia findById(Integer id) {
         log.info("PROVINCIA SERVICIO REMOTO: Búsqueda de provincia con id: " +id);
         ResponseEntity<Provincia> provinciaResponse = this.restTemplate.getForEntity(endpoint + "/id/{id}", Provincia.class, id);
         return provinciaResponse.getBody();
     }
 
+    @CircuitBreaker(name = "provBreaker", fallbackMethod = "getProvinciaInfoFallBack")
     public Provincia findByCod(String cod) {
         log.info("PROVINCIA SERVICIO REMOTO: Búsqueda de provincia con código: " + cod);
         ResponseEntity<Provincia> provinciaResponse = this.restTemplate.getForEntity(endpoint + "/cod/{cod}", Provincia.class, cod);
         return provinciaResponse.getBody();
     }
 
+    @CircuitBreaker(name = "provListBreaker", fallbackMethod = "getListProvinciaInfoFallBack")
     public List<Provincia> findProvinciaByNombre(String nombre) {
         log.info("PROVINCIA SERVICIO REMOTO: Búsqueda de provincias por nombre que contengan: '" + nombre + "'");
         ResponseEntity<Provincia[]> listaP = this.restTemplate.getForEntity(endpoint + "/name/{nombre}", Provincia[].class, nombre);
         return Arrays.asList(listaP.getBody());
+    }
+
+    public Provincia getProvinciaInfoFallBack() {
+        return new Provincia();
+    }
+
+    public List<Provincia> getListProvinciaInfoFallBack() {
+        return new ArrayList<>();
     }
 }
